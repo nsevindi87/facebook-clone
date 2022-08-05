23506668fa3a82ee1f7eeeba058b6411c6d95f7b
Nizami Sevindi <nsevindi87@gmail.com>
2022-08-06 00:11:53 +0200

    Icons is beeing written

--- src/Header.js
+++ src/Header.js
@@ -1,15 +1,40 @@
 import logo from './img/Facebook_f_logo_(2019).svg.png'
 import "./Header.css";
+import {BsSearch} from 'react-icons/bs'
+import {AiFillHome} from 'react-icons/ai'
+import {AiFillFlag} from 'react-icons/ai'
+import {MdOutlineSubscriptions} from 'react-icons/md'
+import {MdOutlineStorefront} from 'react-icons/md'
+import {MdSupervisedUserCircle} from 'react-icons/md'
 
 function Header() {
   return (
     <div className='Header'>
         <div className="header_left">
-        <img src={logo} alt="Logo"/>
+            <img src={logo} alt="Logo"/>
+            <div className="header_input">
+                <BsSearch />
+                <input type="text"/>
+            </div>
+
 
         </div>
         <div className="header_middle">
-
+            <div className="header_option">
+                <AiFillHome fontSize="large" />
+            </div>
+            <div className="header_option">
+                <AiFillFlag fontSize="large" />
+            </div>
+            <div className="header_option">
+                <MdOutlineSubscriptions fontSize="large" />
+            </div>
+            <div className="header_option">
+                <MdOutlineStorefront fontSize="large" />
+            </div>
+            <div className="header_option">
+                <MdSupervisedUserCircle fontSize="large" />
+            </div>
         </div>
         <div className="header_right">
 
