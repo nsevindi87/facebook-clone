c48c49c2ad765294e8b524e02b43737d0e08c538
Nizami Sevindi <nsevindi87@gmail.com>
2022-08-05 22:17:25 +0200

    Header section is being wrotten

--- src/App.js
+++ src/App.js
@@ -4,7 +4,22 @@ function App() {
   return (
     <div className="App">
       <h1>Hallo World</h1>
+
+      {/* Header */}
+
+      {/* App Body */}
+        {/* Sidebar */}
+        {/* Feed */}
+            {/* Stories */}
+            {/* Message */}
+            {/* Post */}
+        {/* Widgets */}
+      
     </div>
+
+    
+
+
   );
 }
 
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD0_EY72GTHbEPc3MXPhKoz8WELwMH-hEI",
+    authDomain: "facebook-clone-1bb6d.firebaseapp.com",
+    projectId: "facebook-clone-1bb6d",
+    storageBucket: "facebook-clone-1bb6d.appspot.com",
+    messagingSenderId: "15265769881",
+    appId: "1:15265769881:web:b7cc94dfde4b7ed3d98b08",
+    measurementId: "G-NHZRZMVG3X"
+  };
