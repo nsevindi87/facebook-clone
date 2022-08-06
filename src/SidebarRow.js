7309a0e5ea5abc4110918d64d6a7ec7f17f207f0
Nizami Sevindi <nsevindi87@gmail.com>
2022-08-07 00:02:09 +0200

    Sidebar is written

---
+++ src/SidebarRow.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./SidebarRow.css"
+
+function SidebarRow({src, Icon, title}) {
+  return (
+    <div className='sidebarRow'>
+        {src}
+        {Icon}    
+        <h4>{title}</h4>
+    </div>
+  )
+}
+
+export default SidebarRow
