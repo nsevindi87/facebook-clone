9e74541ce6c8e97f87c7ccf677fcf68c83650649
Nizami Sevindi <nsevindi87@gmail.com>
2022-08-08 19:38:13 +0200

    Feed section is been written

--- src/Story.js
+++ src/Story.js
@@ -4,7 +4,8 @@ import "./Story.css"
 function Story({image, profileSrc, title}) {
   return (
     <div className='story'>
-      
+      <Avatar src ={profileSrc} />
+      <h4>{title}</h4>
     </div>
   )
 }
