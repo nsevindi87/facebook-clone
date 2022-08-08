a675eeccb3fbafb3b3d9737dcb6a51a8e91005da
Nizami Sevindi <nsevindi87@gmail.com>
2022-08-08 22:35:35 +0200

    Story section is been written

--- src/Story.js
+++ src/Story.js
@@ -1,9 +1,10 @@
-import React from 'react'
-
-function Story() {
+import {Avatar} from "@material-ui/core"
+ 
+function Story({title, profileSrc, image}) {
   return (
-    <div className='story'>
-      <h4>Story</h4>
+    <div style={{backgroundImage: `url(${image})` }} className='story'>
+        <h4>{title}</h4>
+        <Avatar src={profileSrc}/>
     </div>
   )
 }
--- src/StoryReel.js
+++ src/StoryReel.js
@@ -1,11 +1,39 @@
-import React from 'react'
-import Story from './Story'
+import Story from './Story';
+import Profile1 from './img/profile 1.jpeg'
+import Profile2 from './img/profile 2.jpeg'
+import Profile3 from './img/profile 3.jpeg'
+import Profile4 from './img/profile 4.jpeg'
+import Profile5 from './img/profile 5.jpeg'
+import Background1 from './img/Download (1).jpeg'
+import Background2 from './img/Download (2).jpeg'
+import Background3 from './img/Download (3).jpeg'
+import Background4 from './img/Download (4).jpeg'
+import Background5 from './img/Download (5).jpeg'
+
 
 function StoryReel() {
   return (
     <div className='storyreel'>
         <h4>storyreel</h4>
-        <Story />
+        <Story title="george Orwel"
+               profileSrc={Profile1}
+               image={Background1} />
+
+        <Story title="Thomas Edison"
+               profileSrc={Profile2}
+               image={Background2} />
+               
+        <Story title="Andrea Müller"
+               profileSrc={Profile3}
+               image={Background3} />
+
+        <Story title="Sandra Klara"
+               profileSrc={Profile4}
+               image={Background4} />
+
+        <Story title="Claudia Andre"
+               profileSrc={Profile5}
+               image={Background5} />
                 
     </div>
   )
