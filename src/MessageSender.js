d138ccc19cad954764dc49098dbc4b87a4dd9042
Nizami Sevindi <nsevindi87@gmail.com>
2022-08-09 00:29:26 +0200

    Form section is been written

--- src/MessageSender.js
+++ src/MessageSender.js
@@ -1,5 +1,5 @@
 import { Avatar } from '@mui/material';
-import React from 'react';
+import React, { useState } from 'react';
 import './MessageSender.css';
 import VideocamIcon from '@mui/icons-material/Videocam';
 import PhotoLibraryIcon from '@mui/icons-material/PhotoLibrary';
@@ -7,6 +7,9 @@ import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon';
 
 
 function MessageSender() {
+    const [input, setInput] = useState("");
+    const [imageUrl, setImageUrl] = useState("");
+
     const handleSubmit =(e) =>{
         e.preventDefault();
     }
@@ -16,24 +19,28 @@ function MessageSender() {
         <div className="messageSender_top">
             <Avatar/>
             <form>
-                <input placeholder={'What`s on your mind?'} 
+                <input value={input}
+                       onChange={(e)=>setInput(e.target.value)}
+                       placeholder={'What`s on your mind?'} 
                        className="messageSender_input"/>
-                <input placeholder={'image URL (Optional)'} />
+                <input value={imageUrl}
+                       onChange={(e)=>setImageUrl(e.target.value)}
+                       placeholder={'image URL (Optional)'} />
                 <button onClick={handleSubmit} type="submit">
                     Hidden Submit
                 </button>
             </form>
         </div>
         <div className="messageSender_bottom">
-            <div className="message_option">
+            <div className="messagesender_option">
                 <VideocamIcon style={{color:"red"}}/>
                 <h3>Live Video</h3>
             </div>
-            <div className="message_option">
+            <div className="messagesender_option">
                 <PhotoLibraryIcon style={{color:"green"}}/>
                 <h3>Photo/Video</h3>
             </div>
-            <div className="message_option">
+            <div className="messagesender_option">
                 <InsertEmoticonIcon style={{color:"orange"}}/>
                 <h3>Feeling/Activity</h3>
             </div>
