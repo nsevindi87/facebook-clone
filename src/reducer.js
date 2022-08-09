67e2274ebc7ba1773b37b693525b36a29500070b
Nizami Sevindi <nsevindi87@gmail.com>
2022-08-09 21:43:38 +0200

    reducer is written

---
+++ src/reducer.js
@@ -0,0 +1,24 @@
+import { red } from "@material-ui/core/colors";
+
+export const initialState={
+    user:null,
+};
+
+export const actionTypes={
+    SET_USER: "SET_USER",
+};
+
+const reducer = (state,action) =>{
+    console.log(action);
+    switch (action.type){
+        case actionTypes.SET_USER:
+        return{
+            ...state,
+            user: action.user,
+        };
+        default:
+            return state;
+    }
+};
+
+export default reducer;
