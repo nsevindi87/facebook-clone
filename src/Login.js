00cb536e60ff602500c5809635321f6fb5c72bc3
Nizami Sevindi <nsevindi87@gmail.com>
2022-08-09 20:26:10 +0200

    Google auth is connected (firebase)

--- src/Login.js
+++ src/Login.js
@@ -1,14 +1,30 @@
+import { Button } from '@mui/material'
 import logo from './img/fblogo.png'
 import logotext from './img/fbtextlogo.png'
+import './Login.css'
+import  {auth, provider} from "./firebase"
 
 
 function Login() {
+
+    const signIn =()=>{
+        //sign in...
+        auth
+        .signInWithPopup(provider)
+        .then((result) =>{
+            console.log(result)
+        })
+        .catch((error)=> alert(error.message))
+    }
   return (
     <div className="login">
         <div className="login_logo">
             <img src={logo} alt="" />
             <img src={logotext} alt="" />
         </div>
+        <Button type='submit' onClick={signIn}>
+            SIGN IN
+        </Button>
     </div>
   )
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,7 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
 const firebaseConfig = {
     apiKey: "AIzaSyD0_EY72GTHbEPc3MXPhKoz8WELwMH-hEI",
     authDomain: "facebook-clone-1bb6d.firebaseapp.com",
@@ -7,3 +11,13 @@ const firebaseConfig = {
     appId: "1:15265769881:web:b7cc94dfde4b7ed3d98b08",
     measurementId: "G-NHZRZMVG3X"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
